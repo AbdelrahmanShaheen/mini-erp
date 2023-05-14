6b39a39f2cc9a4fd874120a2e9942a78bc8d63c3
Abdelrahman Shaheen <shaheenabdelrahman28@gmail.com>
2023-05-14 16:08:24 +0300

    Create task endpoint

--- routers/task.js
+++ routers/task.js
@@ -1,5 +1,33 @@
 const prisma = require("../prisma/prisma");
+const jwt = require("jsonwebtoken");
 const express = require("express");
+const bcrypt = require("bcrypt");
+const auth = require("../middleware/auth");
 const router = new express.Router();
+//Create task endpoint
+router.post("/tasks", auth, async (req, res) => {
+  try {
+    // Check if the authenticated user has the privilege to create tasks
+    if (req.employee.role !== "SUPER_ADMIN" && req.employee.role !== "HR") {
+      return res.status(403).json({ error: "Access denied" });
+    }
+
+    const { name, deadline, employeeId, salary } = req.body;
+
+    const task = await prisma.task.create({
+      data: {
+        name,
+        deadline: new Date(deadline),
+        employee: { connect: { id: employeeId } },
+        salary,
+      },
+    });
+
+    return res.json(task);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 module.exports = router;
